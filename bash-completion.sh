317793190c6a69838bdddd3e4e17d33fbb090a4d
4ce <88108711+4cecoder@users.noreply.github.com>
2023-01-30 19:46:39 -0500

    Update bash-completion.sh
    try to fix the direct path

--- bash-completion.sh
+++ bash-completion.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/ziggy
 curl https://raw.githubusercontent.com/ziglang/shell-completions/master/_zig -o ~/.config/ziggy/_zig
 
 # Add the .config/ziggy directory to the $fpath
-echo "fpath=(~/.config/ziggy \$fpath)" >> ~/.bashrc
+echo "source \"~/.config/ziggy/_zig\"" >> /.bashrc
 
 # Source the ~/.bashrc file to update the current terminal
 source ~/.bashrc
