0409a9751a0cb92132a23cfe99da5fd648b05dd6
4ce <88108711+4cecoder@users.noreply.github.com>
2023-01-30 19:24:07 -0500

    Create bash-completion.sh

---
+++ bash-completion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make the .config/ziggy directory
+mkdir -p ~/.config/ziggy
+
+# Download the _zig file to the .config/ziggy directory
+curl https://raw.githubusercontent.com/ziglang/shell-completions/master/_zig -o ~/.config/ziggy/_zig
+
+# Add the .config/ziggy directory to the $fpath
+echo "fpath=(~/.config/ziggy \$fpath)" >> ~/.bashrc
+
+# Source the ~/.bashrc file to update the current terminal
+source ~/.bashrc
